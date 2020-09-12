c8a56ad2808b441c45696101b06dbb65cd472c3e
Harish Agrawal <harish@zb.io>
2020-09-12 12:00:42 +0530

    Script to test ballot api

---
+++ ballot/test_api.sh
@@ -0,0 +1,11 @@
+set -x
+sudo apt-get install jq
+echo 'Before vote'
+curl roost-controlplane:30080 | jq
+echo 'Vote now'
+curl -d '{
+       "candidate_id": "roost",
+       "voter_id": "unique_id_123"
+}' -H 'Content-Type: application/json' roost-controlplane:30080 | jq
+echo 'After Voting'
+curl roost-controlplane:30080 | jq
